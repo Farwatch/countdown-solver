63b2bcb0af50536d56f1247830311bd58c2f1109
Farwatch <nick.william.baker@gmail.com>
2017-04-14 17:17:09 +0100

    adds assertj and junit to pom. adds gitignore file

---
+++ src/test/java/SolverServiceTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by nick on 14/04/17.
+ */
+public class SolverServiceTest {
+
+    @Test
+    public void shouldBeTrue(){
+        assertThat(true).isTrue();
+    }
+}
